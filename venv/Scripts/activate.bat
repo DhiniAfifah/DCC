24a6f1df3f6f6b3cca5609ead1e85da778ec1eee
rachellestephanie <rachelle.stephanie@student.umn.ac.id>
2025-03-03 08:58:55 +0700

    fix: use venv and uvicorn

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\dhini\Desktop\DCC\venv
+set VIRTUAL_ENV=C:\Users\a516e\Documents\GitHub\DCC\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
