78a51ad0239382366629a9805fedc8b9842d322b
rachellestephanie <rachelle.stephanie@student.umn.ac.id>
2025-06-16 16:01:17 +0700

    feat: dashboard

---
+++ frontend/components/dashboard.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useLanguage } from "@/context/LanguageContext";
+
+export default function About() {
+    const { t } = useLanguage();
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
