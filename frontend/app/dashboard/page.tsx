3b926bbcb5882082c026162fc73580c594c9ee45
rachellestephanie <rachelle.stephanie@student.umn.ac.id>
2025-06-26 13:57:15 +0700

    feat: dashboard

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,32 @@
+import { columns, Certificate } from "./columns"
+import { DataTable } from "./data-table"
+
+async function getData(): Promise<Certificate[]> {
+  // Fetch data from your API here.
+  return [
+    {
+        id: "S.24-2304",
+        date: "2024-12-19",
+        object: "Standard Resistor",
+        submitter: "Nibras Fitrah Yayiende, M.T.",
+        status: "pending",
+    },
+    {
+        id: "S.24-1349",
+        date: "2024-07-26",
+        object: "Digital Multimeter",
+        submitter: "Agah Faisal, M.Sc",
+        status: "pending",
+    },
+  ]
+}
+
+export default async function Dashboard() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-20">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
